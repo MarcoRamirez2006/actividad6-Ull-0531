7575b80018a528c2068cba9d30e8b9c93079b0ba
MARCO_ANTONIO_RAMIREZ_AMADOR <a.21308051280531@cbtis128.edu.mx>
2024-05-21 01:36:56 +0000

    actividad6-android

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,198 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'SECUNDARIA 128',
+      home: MyHomePage(title: 'SECUNDARIA'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: PageView(
+        children: [
+          //primera pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  'https://raw.githubusercontent.com/MarcoRamirez2006/imggihub/main/DIARIO.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            "UNIFORME",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_circle_right),
+                          Text('uniformes'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //Segunda pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  'https://raw.githubusercontent.com/MarcoRamirez2006/imggihub/main/ins.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'INSCRIPCIONES',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.drive_eta),
+                          Text(
+                            'inscripciones',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Tercera pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  'https://raw.githubusercontent.com/MarcoRamirez2006/imggihub/main/mat.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'MATERIAS',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Text(
+                            'materias',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //Cuarta pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  '',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'TALLERES',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Text(
+                            'TALLERES',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
